bd7951fe5e99c5888bd120d5d432764dd49563bc
Алексей Компанец <trueflywood@gmail.com>
2023-08-27 16:10:04 +0300

    изменение числа кнопками

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View view) {
+        Toast.makeText(this, getString(R.string.button1Text) + " (" + String.valueOf(++counter) + ")" , Toast.LENGTH_SHORT).show();
+    }
+    public void click2(View view) {
+        Toast.makeText(this, getString(R.string.button1Text) + " (" + String.valueOf(--counter) + ")", Toast.LENGTH_SHORT).show();
     }
 }
